2471b697f4061bdafd8fb5fb6a1b8273addb6199
puranikvinit <kvp933.vinit@gmail.com>
2024-05-29 08:24:32 +0530

    remove standard library dependencies

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,9 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
-
 int main() {
-    printf("Kernel Loaded!\n");
+    // printf("Kernel Loaded!\n");
     while(1);
 
-    return EXIT_SUCCESS;
+    return 0;
 }
