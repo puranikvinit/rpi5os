cc1f20f4292d06e3d3027826279f811e23446db2
puranikvinit <kvp933.vinit@gmail.com>
2024-06-03 10:26:16 +0530

    implement PL011 UART

---
+++ kernel/include/uart.h
@@ -0,0 +1,85 @@
+#ifndef UART_H
+#define UART_H
+
+#include "mmio.h"
+
+// ARM PrimeCell (PL011) UART (Revision r1p5) Implementation
+
+// GPIO pin mapping:
+// GPIO function selected for this implementation - a4
+// UART1_TX - GPIO pin 14
+// UART1_RX - GPIO pin 15
+// UART1_CTS - GPIO pin 16
+// UART1_RTS - GPIO pin 17
+
+// Register addresses extracted from the Raspberry Pi 5 Devicetree
+
+enum {
+  UART_CLOCK_FREQ =
+      0x2faf080, // Clock Frequency of dedicated UART clock (clk_uart) - 50MHz
+
+  // Base address of the UART0 registers
+  UART1_BASE = BAR1_BASE + (0x40030000 - MAP_START),
+  UART1_CR = UART1_BASE + 0x030, // UART Control Register (WIDTH: 16 bits)
+  // [9] RXE - RX enable
+  // [8] TXE - TX enable
+  // [0] UARTE - UART enable
+
+  UART1_IBRD =
+      UART1_BASE + 0x024, // Integer Baud Rate Register (WIDTH: 16 bits)
+  UART1_FBRD =
+      UART1_BASE + 0x028, // Fractional Baud Rate Register (WIDTH: 6 bits)
+  UART1_LCR_H = UART1_BASE + 0x02c, // Line Control Register (WIDTH: 8 bits)
+
+  UART1_DR = UART1_BASE + 0x000, // Data Register (WIDTH: 8 bits)
+  UART1_FR = UART1_BASE + 0x018, // Flag Register (WIDTH: 9 bits)
+                                 // [7] TXFE - TX FIFO empty
+                                 // [6] RXFF - RX FIFO full
+                                 // [5] TXFF - TX FIFO full
+                                 // [4] RXFE - RX FIFO empty
+                                 // [3] BUSY - UART busy
+};
+
+/*! \brief Structure to store the baud rate values.
+ *
+ *  \var `baud_rate_integer` The integer part of the baud rate divisor.
+ *  \var `baud_rate_fraction` The fractional part of the baud rate divisor.
+ * */
+
+typedef struct {
+  unsigned int baud_rate_integer;
+  unsigned int baud_rate_fraction;
+} uart_baud_rate_t;
+
+// Member APIs
+
+/*! \brief Calculate the baud rate divisor values for a given baud rate.
+ *
+ * \param `baud_rate` The baud rate to calculate the values for.
+ *
+ * \return The integer and fractional baud rate divisor values.
+ */
+uart_baud_rate_t _calculate_baud_values(long baud_rate);
+
+/*! \brief Initialize the UART0 interface. */
+void uart_init();
+
+/*! \brief Transmit a character over the UART0 interface.
+ *
+ * \param `c` The character to transmit.
+ */
+void uart_putc(char c);
+
+/*! \brief Receive a character over the UART0 interface.
+ *
+ * \return The character received.
+ */
+char uart_getc();
+
+/*! \brief Transmit a string over the UART0 interface.
+ *
+ * \param `s` The pointer to the string to be transmitted.
+ */
+void uart_puts(const char *s);
+
+#endif // !UART_H
---
+++ kernel/src/uart.c
@@ -0,0 +1,83 @@
+#include "uart.h"
+#include "gpio.h"
+#include "mmio.h"
+
+uart_baud_rate_t _calculate_baud_rate(long baud_rate) {
+  double baud_rate_divisor = (double)UART_CLOCK_FREQ / (0x10 * baud_rate);
+  unsigned int baud_rate_integer = (unsigned int)baud_rate_divisor;
+  unsigned int baud_rate_fraction =
+      (unsigned int)((baud_rate_divisor - baud_rate_integer) * 0x40 + 0.5);
+
+  uart_baud_rate_t uart_baud_rate = {.baud_rate_integer = baud_rate_integer,
+                                     .baud_rate_fraction = baud_rate_fraction};
+  return uart_baud_rate;
+}
+
+void uart_init() {
+  // GPIO14 - UART1_TX
+  // GPIO Pin Function = a4
+  // Pull-up = 0
+  // Pull-down = 0
+  // Output disable = 0
+  // Input enable = 1
+  gpio_init(14, 4, 0, 0, 0, 1);
+  // GPIO15 - UART1_RX
+  // GPIO Pin Function = a4
+  // Pull-up = 0
+  // Pull-down = 0
+  // Output disable = 0
+  // Input enable = 1
+  gpio_init(15, 4, 0, 0, 0, 1);
+
+  // NOTE: Flow control not yet implemented, hence the flow control pins are not
+  // initialized.
+
+  // Disable UART interface
+  unsigned int uart_control = mmio_read_32(UART1_CR);
+  uart_control &= ~(1); // Clear the UART enable[0] bit
+  mmio_write_32(UART1_CR, uart_control);
+
+  // Set the baud rate values
+  uart_baud_rate_t uart_baud_rate = _calculate_baud_rate(9600);
+  mmio_write_32(UART1_IBRD, uart_baud_rate.baud_rate_integer);
+  mmio_write_32(UART1_FBRD, uart_baud_rate.baud_rate_fraction);
+
+  // Set the line control register
+  unsigned int uart_lcr_h = mmio_read_32(UART1_LCR_H);
+  uart_lcr_h &= ~(7 << 4);
+  uart_lcr_h |= (3 << 5); // 8-bit word length
+  uart_lcr_h |= (1 << 4); // enable FIFO buffer
+  mmio_write_32(UART1_LCR_H, uart_lcr_h);
+
+  // Enable UART interface
+  uart_control &= ~(1 | (3 << 8));
+  uart_control |= (1 | (3 << 8)); // Set UART enable[0], TX[8] and RX[9] bits.
+  mmio_write_32(UART1_CR, uart_control);
+}
+
+void uart_putc(char c) {
+  // Wait until the UART is not busy
+  // while (mmio_read(UART1_FR) & (1 << 3))
+  // ;
+  // Wait until the TX FIFO is not full
+  while (mmio_read_32(UART1_FR) & (1 << 5))
+    ;
+
+  mmio_write_32(UART1_DR, c);
+}
+
+char uart_getc() {
+  // Wait until the UART is not busy
+  // while (mmio_read(UART1_FR) & (1 << 3))
+  // ;
+  // Wait until the RX FIFO is not empty
+  while (mmio_read_32(UART1_FR) & (1 << 4))
+    ;
+
+  return (mmio_read_32(UART1_DR) & 0xff);
+}
+
+void uart_puts(const char *s) {
+  for (int i = 0; s[i] != '\0'; i++)
+    uart_putc((char)s[i]);
+}
