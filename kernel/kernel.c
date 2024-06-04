8e4f393d3e4ba600555dddf60ac6db0fd7eb5491
puranikvinit <kvp933.vinit@gmail.com>
2024-06-04 17:05:34 +0530

    test string functions in kernel main

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,11 +1,19 @@
+#include "core.h"
+#include "string.h"
 #include "uart.h"
+
+char s[40] = "Hello, from Raspberry Pi 5!\n";
+char buff[32] = "";
 
 int main() {
   uart_init();
-  char *s = "Hello, from Raspberry Pi 5!\n";
+
+  uart_puts(s);
+
+  int_to_str(get_current_exception_level(), buff);
+  uart_puts(buff);
 
   while (1) {
-    uart_puts(s);
   }
 
   return 0;
