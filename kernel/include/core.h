6e28800b0c6f9376b8894bd724b4cd7be82e8806
puranikvinit <kvp933.vinit@gmail.com>
2024-06-04 17:06:07 +0530

    [core]add function to get current exception level of the processor

---
+++ kernel/include/core.h
@@ -0,0 +1,12 @@
+#ifndef CORE_H
+#define CORE_H
+
+// Member APIs
+
+/* \brief Get the current exception level
+ *
+ * \return The current exception level
+ */
+extern int get_current_exception_level();
+
+#endif // !CORE_H
