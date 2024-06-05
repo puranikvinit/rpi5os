423f125c94850f706f2bd8c4cc133fae887d47c7
puranikvinit <kvp933.vinit@gmail.com>
2024-06-05 16:50:41 +0530

    support multicore execution

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,17 +1,25 @@
-#include "core.h"
 #include "string.h"
 #include "uart.h"
 
-char s[40] = "Hello, from Raspberry Pi 5!\n";
-char buff[32] = "";
+long semaphore = 0;
 
-int main() {
-  uart_init();
+int kernel_main(unsigned int core_id) {
+  if (core_id == 0) {
+    uart_init();
+  }
 
-  uart_puts(s);
+  uart_puts("Core ID, \0");
+  uart_putc(core_id + '0');
+  uart_putc('\n');
 
-  int_to_str(get_current_exception_level(), buff);
+  char buff[5] = "";
+  int_to_str(53, buff);
   uart_puts(buff);
+
+  while (semaphore != core_id) {
+  }
+
+  semaphore++;
 
   while (1) {
   }
