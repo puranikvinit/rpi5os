07489050745158232749f978d21895a0356c2b5b
puranikvinit <kvp933.vinit@gmail.com>
2024-06-10 19:55:18 +0530

    add base address for peripherals directly on the SoC

--- kernel/include/mmio.h
+++ kernel/include/mmio.h
@@ -7,6 +7,9 @@ enum {
   BAR1_BASE = 0x1f00000000, // System Base address to which APB0 PERIPHERALS
                             // base address of RP1 are mapped
   MAP_START = 0x40000000, // Peripheral Base in the RP1 Processor Address space
+
+  SOC_PERIPHERAL_BASE = 0x107c000000, // Base address of the peripherals which
+                                      // are on the SoC directly
 };
 
 // Member APIs
