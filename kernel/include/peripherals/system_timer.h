5dd12570efceccebc2f2d3357b04b3477b68cde7
puranikvinit <kvp933.vinit@gmail.com>
2024-06-11 18:16:07 +0530

    implement bcm2835 system timer

--- kernel/include/peripherals/system_timer.h
+++ kernel/include/peripherals/system_timer.h
@@ -2,6 +2,9 @@
 #define SYSTEM_TIMER_H
 
 #include "mmio.h"
+
+#define SYSTEM_TIMER_TRIGGER_INTERVAL 200000
+
 // Register addresses for the Raspberry Pi 5 System Timer
 
 // Register base addresses extracted from the Raspberry Pi 5 Devicetree
@@ -18,18 +21,42 @@ enum {
 
   SYSTEM_TIMER_LO =
       SOC_PERIPHERAL_BASE + 0x3004, // The lower 32 bits of the timer counter.
-  SYSTEM_TIMER_C1 = SOC_PERIPHERAL_BASE + 0x3010,
-  SYSTEM_TIMER_C3 = SOC_PERIPHERAL_BASE + 0x3018, // System Timer Compare 3
+  // NOTE: The higher 32 bits of the timer counter are not accessible.
 
-  SYSTEM_MASKBIT = 3,
+  // The lower 32 bits of the free running counter is matched against the
+  // following compare registers, one for each line.
+  SYSTEM_TIMER_C0 =
+      SOC_PERIPHERAL_BASE + 0x300c, // System Timer Compare for channel 0
+  SYSTEM_TIMER_C1 =
+      SOC_PERIPHERAL_BASE + 0x3010, // System Timer Compare for channel 1
+  SYSTEM_TIMER_C2 =
+      SOC_PERIPHERAL_BASE + 0x3014, // System Timer Compare for channel 2
+  SYSTEM_TIMER_C3 =
+      SOC_PERIPHERAL_BASE + 0x3018, // System Timer Compare for channel 3
 
-  SYSTEM_TIMER_IRQ_0 = 1 << 0,
-  SYSTEM_TIMER_IRQ_1 = 1 << 1,
-  SYSTEM_TIMER_IRQ_2 = 1 << 2,
-  SYSTEM_TIMER_IRQ_3 = 1 << 3,
+  // Status bit for channel 1
+  SYSTEM_TIMER_MASKBIT = 1 << 1,
+
+  SYSTEM_TIMER_IRQ_0 = 64 + 32, // The IRQ ID for channel 0
+  SYSTEM_TIMER_IRQ_1 = 65 + 32, // The IRQ ID for channel 1
+  SYSTEM_TIMER_IRQ_2 = 66 + 32, // The IRQ ID for channel 2
+  SYSTEM_TIMER_IRQ_3 = 67 + 32, // The IRQ ID for channel 3
+  // For the above channel lines, the GIC_SPI IDs have been extracted from the
+  // devicetree of the Raspberry Pi 5 The IRQ IDs are calculated by adding 32 to
+  // the GIC_SPI IDs (as the first 32 IRQs are reserved for SGIs and PPIs)
 };
 
 // Member APIs
-extern void timer_init();
+
+/*! \brief Initialize the System Timer. This function initializes the System
+ * Timer to generate an interrupt after 200ms from the time of execution of the
+ * initialiser.
+ */
+void timer_init();
+
+/*! \brief Handle the System Timer interrupt. This function is called when the
+ * System Timer generates an interrupt.
+ */
+void handle_timer_irq();
 
 #endif // !SYSTEM_TIMER_H
---
+++ kernel/src/peripherals/system_timer.c
@@ -0,0 +1,21 @@
+#include "peripherals/system_timer.h"
+#include "mmio.h"
+#include "peripherals/uart.h"
+
+unsigned int curVal = 0;
+
+void timer_init() {
+  // Set the interval for the timer.
+  curVal = mmio_read_32(SYSTEM_TIMER_LO);
+  curVal += SYSTEM_TIMER_TRIGGER_INTERVAL;
+  mmio_write_32(SYSTEM_TIMER_C1, curVal);
+}
+
+void handle_timer_irq() {
+  // Increment the current value of the timer by the interval, and set the
+  // timer.
+  curVal += SYSTEM_TIMER_TRIGGER_INTERVAL;
+  mmio_write_32(SYSTEM_TIMER_C1, curVal);
+  mmio_write_32(SYSTEM_TIMER_CTRL_STATUS, SYSTEM_TIMER_MASKBIT);
+  uart_puts("Timer interrupt received\n\0");
+}
