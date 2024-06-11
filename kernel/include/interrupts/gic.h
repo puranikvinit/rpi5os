5f193712d62aaef4c5df59ba57a37cde0dcd44c2
puranikvinit <kvp933.vinit@gmail.com>
2024-06-11 18:14:54 +0530

    [gic]add inline documentation and add interrupt handler

--- kernel/include/interrupts/gic.h
+++ kernel/include/interrupts/gic.h
@@ -9,8 +9,12 @@
 // Controller" Technical Reference Manual
 
 enum {
+  // Base address of the GIC-400 General Interrupt Controller
+  GIC_BASE = 0x107fff8000,
+
   // GIC-400 Distributor Register
-  GIC_D_BASE = 0x107fff9000, // Base address of the GIC-400 Distributor Register
+  GIC_D_BASE =
+      GIC_BASE + 0x1000, // Base address of the GIC-400 Distributor Register
 
   GIC_D_CTLR = GIC_D_BASE + 0x000, // Distributor Control Register
 
@@ -26,7 +30,7 @@ enum {
 
   // GIC-400 CPU Interface Register
   GIC_C_BASE =
-      0x107fffa000, // Base address of the GIC-400 CPU Interface Register
+      GIC_BASE + 0x2000, // Base address of the GIC-400 CPU Interface Register
 
   GIC_C_CTLR = GIC_C_BASE + 0x000,     // CPU Interface Control Register
   GIC_C_PRI_MASK = GIC_C_BASE + 0x004, // Interrupt Priority Mask Register
@@ -35,17 +39,29 @@ enum {
 
   GIC_NUM_SPI = 192, // Number of SPIs configured in the GIC-400 in the SoC
   GIC_NUM_IRQ = GIC_NUM_SPI + 32, // Total number of IRQs in the GIC-400
+  // The first 32 IRQs are reserved for the CPU internal exceptions (SGIs -
+  // Software Generated Interrupts and PPIs - Private Peripheral Interrupts)
 
   GIC_SPURIOUS_INTERRUPT = 1023, // Spurious interrupt number
 };
 
 // Member APIs
 
+/*! \brief Initialize the GIC-400 General Interrupt Controller. This function
+ * initializes the GIC-400 Distributor and CPU Interface Registers to enable the
+ * interrupt handling.
+ */
 void gic_init();
-void register_irq_handler(unsigned int irq);
-void gic_dispatch();
-// void enable_interrupt(unsigned int irq);
-// void assign_target(unsigned int irq, unsigned int cpu);
-// void enable_interrupt_controller();
+
+/*! \brief Enable an IRQ line in the GIC-400 Distributor Register.
+ *
+ * \param `irq` The IRQ line to enable.
+ */
+void enable_irq_line(unsigned int irq);
+
+/*! \brief Handle IRQs when the corresponding interrupt is raised.
+ * This function (as of now) handles EL1h_IRQ and EL0_64_IRQ interrupts.
+ */
+void handle_irq();
 
 #endif // !GIC_H
--- kernel/src/interrupts/gic.c
+++ kernel/src/interrupts/gic.c
@@ -2,75 +2,71 @@
 #include "mmio.h"
 #include "peripherals/system_timer.h"
 #include "peripherals/uart.h"
-#include "util/string.h"
 
 void gic_init() {
+  // Disable all interrupt lines.
   for (int i = 0; i < GIC_NUM_IRQ >> 5; i++) {
-    // mmio_write_32(GIC_D_DISABLE + 4 * i, 0xffffffff);
-
-    mmio_write_32(GIC_D_DISABLE + 4 * i, 0);
+    mmio_write_32(GIC_D_DISABLE + 4 * i, 0xffffffff);
   }
-  uart_puts("a\n\0");
 
+  // Set priority mask register to the lowest value possible, to allow all
+  // interrupts to be raised.
   mmio_write_32(GIC_C_PRI_MASK, 0xff);
-  uart_puts("b\n\0");
 
+  // Set the priority of all interrupts to be greater than the priority mask, or
+  // else they will not be raised.
   for (int i = 0; i < GIC_NUM_IRQ >> 2; i++) {
     mmio_write_32(GIC_D_PRI + 4 * i, 0x7f7f7f7f);
   }
-  uart_puts("c\n\0");
 
+  // BIT[1] Set all interrupts to be level sensitive; active high.
+  // BIT[0] RES1 (Specified handling mode of peripheral interrupts in primitive
+  // architectures).
   for (int i = 0; i < GIC_NUM_IRQ >> 4; i++) {
     mmio_write_32(GIC_D_CONFIG + 4 * i, 0x55555555);
   }
-  uart_puts("d\n\0");
 
+  // Set all interrupts to be handled by all CPUs. (Specify target to be all the
+  // CPU cores).
+  // The first 32 bits are RES0.
   for (int i = 0; i < GIC_NUM_IRQ >> 2; i++) {
     if (i < 8)
       continue;
     mmio_write_32(GIC_D_TARGET + 4 * i, 0xffffffff);
   }
-  uart_puts("e\n\0");
 
+  // Enable the GIC Distributor and CPU Interface.
   mmio_write_32(GIC_D_CTLR, 0x1);
-  uart_puts("f\n\0");
-
   mmio_write_32(GIC_C_CTLR, 0x1);
-  uart_puts("g\n\0");
 }
 
-void register_irq_handler(unsigned int irq) {
-  unsigned int val = 0x1 << (irq & 0x1f);
-  unsigned int n = irq >> 5;
+void enable_irq_line(unsigned int irq) {
+  // Calculate the bit position of the interrupt line.
+  unsigned int enable_bit = 0x1 << (irq & 0x1f);
 
-  mmio_write_32(GIC_D_ENABLE + 4 * n, val);
+  // Calculate the register offset of the interrupt line.
+  unsigned int register_offset = irq >> 5;
+
+  mmio_write_32(GIC_D_ENABLE + 4 * register_offset, enable_bit);
 }
 
-void gic_dispatch() {
-  while (1) {
-    unsigned int interrupt_spi = mmio_read_32(GIC_C_ACK);
-    unsigned int interrupt_id = interrupt_spi & 0x3FF;
-    uart_puts("Dispatch Triggered!\n\0");
+void handle_irq() {
+  // Read the interrupt ID from the GIC CPU Interface interrupt ACKNOWLEDGE
+  // register.
+  unsigned int interrupt_spi = mmio_read_32(GIC_C_ACK);
+  unsigned int interrupt_id =
+      interrupt_spi & 0x3ff; // Extract the interrupt ID, ignoring the CPU ID.
 
-    if (interrupt_id >= GIC_NUM_IRQ) {
-      uart_puts("Spurious interrupt received: \0");
-      char buff[9] = "";
-      int_to_str(interrupt_id, buff);
-      uart_puts(buff);
-      uart_putc('\n');
-      timer_init();
-      uart_puts("Timer Initialized!\n\0");
+  // Trigger the respective interrupt handlers.
+  switch (interrupt_id) {
+  case SYSTEM_TIMER_IRQ_1:
+    mmio_write_32(GIC_C_EOI, interrupt_id);
+    handle_timer_irq();
+    break;
 
-      __asm__("msr daif, xzr\n\t"
-              "wfi\n\t");
-      continue;
-    }
-
-    if (interrupt_id == 30) {
-      uart_puts("Timer interrupt received!\n\0");
-    } else {
-      uart_puts("Unknown interrupt received!\n\0");
-    }
-    mmio_write_32(GIC_C_EOI, interrupt_spi);
+  default:
+    mmio_write_32(GIC_C_EOI, interrupt_id);
+    uart_puts("Unknown interrupt received!\n\0");
+    break;
   }
 }
