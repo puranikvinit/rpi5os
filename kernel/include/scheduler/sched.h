65143700e4685273bb6416a635d646e427c9d070
puranikvinit <kvp933.vinit@gmail.com>
2024-06-12 18:12:43 +0530

    implement simple process scheduler

---
+++ kernel/include/scheduler/sched.h
@@ -0,0 +1,85 @@
+#ifndef SCHED_H
+#define SCHED_H
+
+#define THREAD_SIZE 4096
+
+#define MAX_TASKS 64
+
+#define FIRST_TASK task[0]
+#define LAST_TASK task[MAX_TASKS - 1]
+
+typedef enum {
+  TASK_RUNNING,
+  TASK_INTERRUPTIBLE,
+  TASK_UNINTERRUPTIBLE,
+  TASK_STOPPED,
+} task_state_t;
+
+typedef struct {
+  unsigned long x19;
+  unsigned long x20;
+  unsigned long x21;
+  unsigned long x22;
+  unsigned long x23;
+  unsigned long x24;
+  unsigned long x25;
+  unsigned long x26;
+  unsigned long x27;
+  unsigned long x28;
+  unsigned long fp; // x29
+  unsigned long pc; // x30
+  unsigned long sp;
+} cpu_context_t;
+
+typedef struct {
+  cpu_context_t cpu_context;
+  task_state_t state;
+  long counter;
+  long priority;
+  long skip_preempt_count;
+} task_struct_t;
+
+extern task_struct_t *current_task;
+extern task_struct_t *task[MAX_TASKS];
+extern int number_of_tasks;
+
+#define INIT_TASK                                                              \
+  {                                                                            \
+      .cpu_context =                                                           \
+          {                                                                    \
+              .x19 = 0,                                                        \
+              .x20 = 0,                                                        \
+              .x21 = 0,                                                        \
+              .x22 = 0,                                                        \
+              .x23 = 0,                                                        \
+              .x24 = 0,                                                        \
+              .x25 = 0,                                                        \
+              .x26 = 0,                                                        \
+              .x27 = 0,                                                        \
+              .x28 = 0,                                                        \
+              .fp = 0,                                                         \
+              .pc = 0,                                                         \
+              .sp = 0,                                                         \
+          },                                                                   \
+      .state = TASK_RUNNING,                                                   \
+      .counter = 0,                                                            \
+      .priority = 1,                                                           \
+      .skip_preempt_count = 0,                                                 \
+  }
+
+// Member APIs
+
+void scheduler_init();
+void schedule();
+
+void preempt_enable();
+void preempt_disable();
+
+void switch_task(task_struct_t *next);
+extern void switch_context(task_struct_t *prev, task_struct_t *next);
+
+void timer_tick();
+
+void task_init();
+
+#endif // !SCHED_H
---
+++ kernel/src/scheduler/sched.c
@@ -0,0 +1,76 @@
+#include "scheduler/sched.h"
+#include "interrupts/vector_init.h"
+
+static task_struct_t init_task = INIT_TASK;
+task_struct_t *current_task = &(init_task);
+task_struct_t *task[MAX_TASKS] = {
+    &init_task,
+};
+int number_of_tasks = 1;
+
+void preempt_enable() { current_task->skip_preempt_count--; }
+
+void preempt_disable() { current_task->skip_preempt_count++; }
+
+void scheduler_init() {
+  current_task->counter = 0;
+  schedule();
+}
+
+void schedule() {
+  preempt_disable();
+
+  int c, next;
+  task_struct_t *p;
+
+  while (1) {
+    c = -1;
+    next = 0;
+    for (int i = 0; i < MAX_TASKS; i++) {
+      p = task[i];
+      if (p && p->state == TASK_RUNNING && p->counter > c) {
+        c = p->counter;
+        next = i;
+      }
+    }
+
+    if (c) {
+      break;
+    }
+
+    for (int i = 0; i < MAX_TASKS; i++) {
+      p = task[i];
+      if (p)
+        p->counter = (p->counter >> 1) + p->priority;
+    }
+  }
+  switch_task(task[next]);
+  preempt_enable();
+}
+
+void switch_task(task_struct_t *next) {
+  if (current_task == next)
+    return;
+
+  task_struct_t *prev = current_task;
+  current_task = next;
+
+  switch_context(prev, current_task);
+}
+
+void timer_tick() {
+  --current_task->counter;
+  if (current_task->counter > 0 || current_task->skip_preempt_count > 0)
+    return;
+
+  current_task->counter = 0;
+  interrupts_enable();
+  schedule();
+  interrupts_disable();
+}
+
+void task_init() {
+  // Any other initialisation that needs to be necessarily done before the task
+  // is preempted.
+  preempt_enable();
+}
